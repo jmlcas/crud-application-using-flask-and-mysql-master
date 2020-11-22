4d55d043ddb16eae7a13855e8f80999a4ad49ee9
José Maria Labarta <59398281+jmlcas@users.noreply.github.com>
2020-11-22 12:48:22 +0100

    Add files via upload

---
+++ source_code/server.py
@@ -0,0 +1,94 @@
+﻿'''
+Created on Jan 10, 2017
+
+@author: hanif
+'''
+
+from flask import Flask, flash, render_template, redirect, url_for, request, session
+from module.database import Database
+
+
+app = Flask(__name__)
+app.secret_key = "mys3cr3tk3y"
+db = Database()
+
+@app.route('/')
+def index():
+    data = db.read(None)
+
+    return render_template('index.html', data = data)
+
+@app.route('/add/')
+def add():
+    return render_template('add.html')
+
+@app.route('/addphone', methods = ['POST', 'GET'])
+def addphone():
+    if request.method == 'POST' and request.form['save']:
+        if db.insert(request.form):
+            flash("Un nuevo contacto ha sido agregado")
+        else:
+            flash("El contacto no ha podido ser agregado")
+
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/update/<int:id>/')
+def update(id):
+    data = db.read(id);
+
+    if len(data) == 0:
+        return redirect(url_for('index'))
+    else:
+        session['update'] = id
+        return render_template('update.html', data = data)
+
+@app.route('/updatephone', methods = ['POST'])
+def updatephone():
+    if request.method == 'POST' and request.form['update']:
+
+        if db.update(session['update'], request.form):
+            flash('El contacto se ha actualizado satisfactoriamente')
+
+        else:
+            flash('El contacto no se ha podido actualizar')
+
+        session.pop('update', None)
+
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>/')
+def delete(id):
+    data = db.read(id);
+
+    if len(data) == 0:
+        return redirect(url_for('index'))
+    else:
+        session['delete'] = id
+        return render_template('delete.html', data = data)
+
+@app.route('/deletephone', methods = ['POST'])
+def deletephone():
+    if request.method == 'POST' and request.form['delete']:
+
+        if db.delete(session['delete']):
+            flash('El contacto se ha eliminado satisfactoriamente')
+
+        else:
+            flash('El contacto no se ha podido eliminar')
+
+        session.pop('delete', None)
+
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html')
+
+if __name__ == '__main__':
+    app.run(port=8181, host="0.0.0.0")
